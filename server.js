edf0283b400b3d24a865b9abb7791fffed64c2f7
Marco <marco.xausa@studenti.unitn.it>
2022-05-17 17:18:17 +0200

    code indentation

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ var port=3000;
 
 app.use('/', router);  
 
-//running web server. Database connection still missing (to be added later on)
-//database part hasn't been implemented because i haven't checked it yet
-
 //username:admin
 //psw: Adminsw
 
-//string must be modified with the password 
-var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority'
-).then ( () => {console.log("Connected to Database"), app.listen(8080, () => { console.log(`Server listening`);})
-}).catch(()=> console.log("Database connection Error"));
+var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority').then ( () => {
+    console.log("Connected to Database"), 
+    app.listen(8080, () => { console.log(`Server listening`);
+})
+}).catch(()=> {
+    console.log("Database connection Error")
+});
 
  
