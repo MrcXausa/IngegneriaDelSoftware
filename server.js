e62f42fdacbe84bf81a3e95690ab1c64473a51c2
Riccardo Gilli <gilli.riccardo@gmail.com>
2022-05-17 18:21:31 +0200

    ENV port param, API versioning, indentation

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express');  
 const router = require('./router.js');
 const app = express();  
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
-var port=3000;
+var port = process.env.PORT || 3000;
 
 // Handling GET requests  
-
-
-app.use('/', router);  
+app.use('/v1', router);  
 
 //username:admin
 //psw: Adminsw
-
-var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority').then ( () => {
-    console.log("Connected to Database"), 
-    app.listen(8080, () => { console.log(`Server listening`);
-})
+var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority').then(() => {
+        console.log("Connected to Database"), 
+        app.listen(8080, () => { console.log(`Server listening`); })
 }).catch(()=> {
     console.log("Database connection Error")
 });
