60fb8baf6f2f7da087f473ccc398775f09b873c0
Mattia <meneghinmattia1@gmail.com>
2022-05-17 10:26:17 +0200

    server fixed

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const router = require('./router.js');
 const app = express();  
 const mongoose =require('mongoose')
 
-
-
 var port=3000;
 
 // Handling GET requests  
@@ -20,7 +18,7 @@ app.use('/', router);
 
 //string must be modified with the password 
 var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority'
-).then ( () => console.log("Connected to Database")).catch(()=> console.log("Database connection Error"));
+).then ( () => {console.log("Connected to Database"), app.listen(8080, () => { console.log(`Server listening`);})
+}).catch(()=> console.log("Database connection Error"));
 
-app.listen(8080, () => { console.log(`Server listening`);
-}) 
+ 
