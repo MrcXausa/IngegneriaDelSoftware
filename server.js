630f7bde096d470d5e4dd4722f495395a6a393b4
Riccardo Gilli <gilli.riccardo@gmail.com>
2022-05-18 09:39:39 +0200

    Added dotenv dependency

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');  
 const router = require('./router.js');
 const app = express();  
@@ -10,7 +12,7 @@ app.use('/v1', router);
 
 //username:admin
 //psw: Adminsw
-var db = mongoose.connect('mongodb+srv://admin:Adminsw@ingegneriasw.shvnz.mongodb.net/?retryWrites=true&w=majority').then(() => {
+var db = mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
         console.log("Connected to Database"), 
         app.listen(8080, () => { console.log(`Server listening`); })
 }).catch(()=> {
