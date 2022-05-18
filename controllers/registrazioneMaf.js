e19ebe712358d11ed1801b77943017ec3e3c50ed
Riccardo Gilli <gilli.riccardo@gmail.com>
2022-05-18 09:45:26 +0200

    comments on code

--- controllers/registrazioneMaf.js
+++ controllers/registrazioneMaf.js
@@ -2,17 +2,21 @@ const Utente = require('../models/Utente')
 
 async function salvaMaf(req, res) {
   const body = req.body;
+  // verifico la presenza di tutti i parametri obbligatori
   if (!body.cognome || !body.nome || !body.codiceFiscale || !body.email) {
     res.status(400).send({success: false, error: 'I campi nome, cognome, codice fiscale e indirizzo e-mail sono obbligatori'})
     return
   }
 
+  // verifico l'esistenza di un altro MAF registrato nel sistema
+  // nel caso non è possibile procedere alla registrazione di un ulteriore MAF
   let dbMaf = await Utente.findOne({ruolo: 'manager'}).exec();
   if (dbMaf) {
     res.status(409).send({success: false, error: 'Esiste già un manager registrato'});
     return
   }
 
+  // creo il mio document con i dati della richiesta
   const utente = new Utente({
     nome: body.nome,
     cognome: body.cognome,
@@ -23,7 +27,7 @@ async function salvaMaf(req, res) {
 
   utente.save()
   .then(() => {
-    res.status(201).send({'success': true, self: '/api/v1/utenti/' + utente._id})
+    res.status(201).send({success: true, self: '/api/v1/utenti/' + utente._id})
   })
   .catch((error) => {
     console.log(error)
