a7e6a92e47b69bf6059e0600daa75e989c1ee867
Marco <marco.xausa@studenti.unitn.it>
2022-05-19 09:02:01 +0200

    setting up swagger

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
