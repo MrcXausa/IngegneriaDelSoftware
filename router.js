115a02ba242b2848ebc5021c83fe8413aec844f0
Marco <marco.xausa@studenti.unitn.it>
2022-05-19 10:10:51 +0200

    changing swagger settings

--- router.js
+++ router.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./apiary.apib');
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
