5c98e1a1121bc0cc671c2fed84bffb020b67c74f
Marco <marco.xausa@studenti.unitn.it>
2022-05-21 12:32:20 +0200

    checkToken and loginMaf implementation

--- controllers/loginMaf.js
+++ controllers/loginMaf.js
@@ -1,26 +1,28 @@
+const { ignore } = require('nodemon/lib/rules');
 const Utente = require('../models/Utente')
 
 
 async function loginMaf(req, res) {
     const body = req.body;
-    //if there's no email
-    if(!body.email){
+    
+    /*
+    if(!body.user){
         res.status(400).send({success: false, error: 'email non inserita'})
         return
     }
-
-    //if the email is present in the request
-    //search for the email in the database
-    let dbMaf = await Utente.findOne({email: body.email, ruolo: 'manager'}).exec();
-    if(dbMaf){//if the email is found
-
-        
-
+    */
+    if(body.user){
+        let dbMaf = await Utente.findOne({email: body.user.email, ruolo: 'manager'}).exec();
+        if(dbMaf){//maf riconosciuto
+            //dummy response
+            res.json({ success: true, message: 'Login avvenuto con successo'}); 
+        }
+        else{//email not fount CHECK CODICE ERRORE
+            res.status(401).send({success: false, error: 'utente non amministratore'})
+            return
+        }
     }
-    else{//email not fount CHECK CODICE ERRORE
-        res.status(500).send({success: false, error: 'email non riconosciuta dal sistema'})
-        return
-    }
+    
 }
 
 module.exports = loginMaf
--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -1,4 +1,5 @@
 const { getAuth } = require('firebase-admin/auth')
+const Utente = require('../models/Utente')
 
 const tokenChecker = function(req, res, next) {  // header or url parameters or post parameters  
     var idToken = req.body.token || req.query.token || req.headers['x-access-token'];  
@@ -7,9 +8,19 @@ const tokenChecker = function(req, res, next) {  // header or url parameters or 
         res.status(401).json({success: false, message: 'La risorsa richiesta richiede l\'autenticazione'})  
 
     //https://firebase.google.com/docs/auth/admin/verify-id-tokens documentation
-    getAuth().verifyIdToken(idToken).then((decodedToken) => {//problema funzione getAuth
-        const uid = decodedToken.uid;
-        // todo: verificare che l'email contenuta nel token corrisponda all'email di un utente. se si, popolare req.user
+    getAuth().verifyIdToken(idToken).then((decodedToken) => {
+        //const uid = decodedToken.uid;
+        //Controllo se la mail nel token è contenuta nel database (corrisponde ad un utente)
+        //se è contenuta allego quell'utente alla risposta
+        //altrimenti ritorno uno stato 401 unauthorized
+
+        mail=decodedToken.email 
+        let user = await Utente.findOne({email: mail}).exec();
+        if(!user)
+            req.status(401).json({success: false, message: 'Email non riconosciuta'});  
+        else
+            req.user=user
+
         next();
     })
     .catch((error) => {//token non verificato
