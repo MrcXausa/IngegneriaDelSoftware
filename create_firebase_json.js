c32e4892cc7ca257d766fcdbd0dc5f82c450aefe
Marco <marco.xausa@studenti.unitn.it>
2022-05-25 15:56:06 +0200

    update script for firebase json configuration

--- create_firebase_json.js
+++ create_firebase_json.js
@@ -2,24 +2,24 @@ require('dotenv').config()
 
 const fs = require('fs')
 
-const createFirebaseJson = ( ) => {
-    let path="/firebase-key.json"
-    let data={
-        type: process.env.UNO,
-        project_id: process.env.DUE,
-        private_key_id: process.env.TRE,
-        private_key: process.env.QUATTRO,
-        client_email: process.env.CINQUE,
-        client_id: process.env.SEI,
-        auth_uri: process.env.SETTE,
-        token_uri: process.env.OTTO,
-        auth_provider_x509_cert_url: process.env.NOVE,
-        client_x509_cert_url: process.env.DIECI
-    }
-  try {
+
+let path="firebase-key.json"
+let data={
+    type: process.env.UNO,
+    project_id: process.env.DUE,
+    private_key_id: process.env.TRE,
+    private_key: process.env.QUATTRO,
+    client_email: process.env.CINQUE,
+    client_id: process.env.SEI,
+    auth_uri: process.env.SETTE,
+    token_uri: process.env.OTTO,
+    auth_provider_x509_cert_url: process.env.NOVE,
+    client_x509_cert_url: process.env.DIECI
+}
+try {
     fs.writeFileSync(path, JSON.stringify(data))
-  } catch (err) {
-    console.error("IMPOSSIBILE SALVARE FILE\N")
+    console.log("file creato")
+} catch (err) {
+    console.log("IMPOSSIBILE SALVARE FILE\N")
     console.error(err)
-  }
 }
