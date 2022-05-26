ada8ce25732cfa78dd597150fe0ea471dc671f1e
Marco <marco.xausa@studenti.unitn.it>
2022-05-26 10:05:17 +0200

    setted up firebase initialization via env variables

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const router = require('./router.js');
 const app = express();  
 const mongoose = require('mongoose')
 const cors = require('cors')
-
+const privateKeyFirebase  = JSON.parse(process.env.QUATTRO)
 const admin = require('firebase-admin');
+//console.log(process.env.QUATTRO)
+//console.log(privateKeyFirebase)
 let firebaseauth={
     type: process.env.UNO,
     project_id: process.env.DUE,
     private_key_id: process.env.TRE,
-    private_key: process.env.QUATTRO,
+    private_key: privateKeyFirebase.privatekey,
     client_email: process.env.CINQUE,
     client_id: process.env.SEI,
     auth_uri: process.env.SETTE,
@@ -20,7 +22,7 @@ let firebaseauth={
     auth_provider_x509_cert_url: process.env.NOVE,
     client_x509_cert_url: process.env.DIECI
 }
-initializeApp(firebaseauth);
+//console.log(firebaseauth)
 
 var port = process.env.PORT || 8080;
 
@@ -31,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', router);  
 
 
-var db = mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
+var db = mongoose.connect(process.env.MONGODB_CONNECTION_STRING_LOCAL).then(() => {
     console.log("Connected to Database"), 
     app.listen(port, () => { console.log(`Server listening`); })
 }).catch(()=> {
