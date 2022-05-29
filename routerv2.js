62ae1115dba15c0bbed13926559b73d06e085fc0
Riccardo Gilli <gilli.riccardo@gmail.com>
2022-05-29 22:39:39 +0200

    Added login arbitro controller and route

--- routerv2.js
+++ routerv2.js
@@ -3,6 +3,7 @@ const routerv2 = express.Router()
 const avviaTorneo=require('./controllers/avviaTorneo')
 const tokenChecker = require('./middlewares/checkToken')
 const {getArbitri, approvaArbitro} = require('./controllers/arbitri.js')
+const loginArbitro = require('./controllers/loginArbitro')
 
 // middleware that is specific to this router
 routerv2.use((req, res, next) => {
@@ -13,5 +14,6 @@ routerv2.use((req, res, next) => {
 routerv2.put('/avviatorneo',tokenChecker,avviaTorneo);
 routerv2.get('/arbitri', tokenChecker, getArbitri);
 routerv2.put('/arbitri/:id/approva', tokenChecker, approvaArbitro);
+routerv1.get('/arbitro', tokenChecker, loginArbitro)
 
 module.exports = routerv2
