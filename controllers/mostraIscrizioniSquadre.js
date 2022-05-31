8f09420de4d23f14211be4fac745a8a393f93c62
Marco <marco.xausa@studenti.unitn.it>
2022-05-31 22:23:55 +0200

    modified htt status codes

--- controllers/mostraIscrizioniSquadre.js
+++ controllers/mostraIscrizioniSquadre.js
@@ -5,7 +5,7 @@ async function mostraIscrizioniSquadre(req,res){
     //trovo tutte le squadre non approvate
     let iscrizioni= await Squadra.find({approvata:'false'});
 
-    if(iscrizioni){//se ci sono iscrizioni non approvate
+    if(iscrizioni){//se ci sono iscrizioni non approvate    
         let torneo= await Torneo.findOne();
         if(torneo && torneo.stato=="attivo")//se esiste un torneo ed è in stato avviato invio le iscrizioni
             res.status(200).json(iscrizioni);
@@ -13,7 +13,7 @@ async function mostraIscrizioniSquadre(req,res){
             res.status(403).send({success: false, error: 'non esiste un torneo in stato avviato'})      
     }
     else{ //se non ci sono iscrizioni da approvare ritorno stato ok ma con testo "nessuna iscrizione"
-        res.status(204).send({success: true, error: 'non ci sono iscrizioni al momento'})
+        res.status(406).send({success: false, error: 'non ci sono iscrizioni al momento'})
         //DA VERIFICARE LA RISPOSTA
     }
 }
