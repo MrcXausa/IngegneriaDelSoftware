18e8a98f1ce8aabc5946aecf8bb8364746423961
Marco <marco.xausa@studenti.unitn.it>
2022-06-01 19:19:33 +0200

    updated router v2

--- routerv2.js
+++ routerv2.js
@@ -2,13 +2,10 @@ const express = require('express')
 const routerv2 = express.Router()
 const avviaTorneo=require('./controllers/avviaTorneo')
 const tokenChecker = require('./middlewares/checkToken')
-<<<<<<< HEAD
 const {getArbitri, approvaArbitro} = require('./controllers/arbitri.js')
-=======
 const mostraIscrizioniSquadre=require("./controllers/mostraIscrizioniSquadre")
 const approvazioneSquadra=require("./controllers/approvazioneSquadra.js")
 const getGiocatore=require("./controllers/getGiocatore")
->>>>>>> approvazioneIscrizione
 
 // middleware that is specific to this router
 routerv2.use((req, res, next) => {
@@ -17,14 +14,10 @@ routerv2.use((req, res, next) => {
 })
 
 routerv2.put('/avviatorneo',tokenChecker,avviaTorneo);
-<<<<<<< HEAD
 routerv2.get('/arbitri', tokenChecker, getArbitri);
 routerv2.put('/arbitri/:id/approva', tokenChecker, approvaArbitro);
-=======
 routerv2.get("/mostraiscrizionisquadre",tokenChecker,mostraIscrizioniSquadre);
 routerv2.put("/approvazionesquadra/:id",tokenChecker,approvazioneSquadra);
 routerv2.get("/giocatore/:id",getGiocatore)
-
->>>>>>> approvazioneIscrizione
 
 module.exports = routerv2
