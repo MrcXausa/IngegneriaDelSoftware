459338f9126d756562046268200c5086811b0376
Marco <marco.xausa@studenti.unitn.it>
2022-06-03 14:57:32 +0200

    updated visualizzazionePartite.js

--- controllers/visualizzazionePartite.js
+++ controllers/visualizzazionePartite.js
@@ -1,12 +1,28 @@
 const Partita = require('../models/Partita')
+const Squadra=require("../models/Squadra")
+const Utente =require("../models/Utente")
 
 // Restituisce TUTTE le partite
 
 async function getPartite(req, res){            
     const partite = await Partita.find({})      // GET TUTT le partite
-    console.log(partite);
 
-    if(partite){                                // Se esiste almeno una partita [partita!=0]
+
+    if(partite){   //se ci sono le partite                             
+        for(var i=0;i<partite.length;i++){ //recupero i dati relativi a squadre a arbitro
+            var casa= await Squadra.findById(partite[i].casa)
+            var ospite =await Squadra.findById(partite[i].ospite)
+            var arbitro= await Utente.findById(partite[i].arbitro)
+
+            if(!arbitro || !casa || !ospite){
+                res.status(400).send({success: false, error: 'Errore nei dati delle partite'})
+                return
+            }
+
+            partite[i].casa=casa.nome;
+            partite[i].ospite=ospite.nome;
+            partite[i].arbitro = arbitro.nome+" "+arbitro.cognome;
+        }
         res.status(200).json(partite);
     }else{                                      // Se non esistono partite [partita=0]
         res.status(404).send({success: false, error: 'Non esistono partite'})
